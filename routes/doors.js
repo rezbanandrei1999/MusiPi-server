bb58b718c5c5d6db2dc083a17c2152b4a994056e
RezbanAndreiTakeOff <andrei.rezban@takeofflabs.com>
2021-04-27 15:44:59 +0300

    add new APIs

--- routes/doors.js
+++ routes/doors.js
@@ -6,24 +6,25 @@ const uri = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 router.get('/doors', function(req, res) {
-  var id = req.body.id;
-  var response = [];
+
+  var response = []
   async function run() {
     try {
       await client.connect();
-      const doors = client.db("musipi").collection("doors");
-      const filter = {$or: [
-        {
-          room_1: ObjectId(id)
-        },
-        {
-          room_2: ObjectId(id)
-        }
-      ]}
 
-      const cursor = doors.find(filter);
+      const doors = client.db("musipi").collection("doors");
+      const rooms = client.db("musipi").collection("rooms");
+      const cursor = doors.find();
+      const cursor2 = rooms.find();
+      var rooms_map = new Map();
+
+      await cursor2.forEach(e => {
+        rooms_map[e._id] = e;
+      })
 
       await cursor.forEach(e => {
+        e.room_1 = rooms_map[e.room_1.toString()]
+        e.room_2 = rooms_map[e.room_2.toString()]
         response.push(e);
       });;
     } finally {
@@ -33,4 +34,101 @@ router.get('/doors', function(req, res) {
   run().catch(console.dir);
 });
 
+router.get('/doors/room', function(req, res) {
+  var id = req.body.id;
+  var response = [];
+  var ids = [];
+  async function run() {
+    try {
+      await client.connect();
+      const doors = client.db("musipi").collection("doors");
+      const rooms = client.db("musipi").collection("rooms");
+      var filter = {$or: [
+        {
+          room_1: ObjectId(id)
+        },
+        {
+          room_2: ObjectId(id)
+        }
+      ]}
+
+      const cursor = await doors.find(filter);
+      await cursor.forEach(e => {
+        var aux = e.room_1;
+        if (aux.toString() != id.toString())
+          ids.push(aux);
+        var aux = e.room_2;
+        if (aux.toString() != id.toString())
+          ids.push(aux);
+      });
+      filter = { _id: { $in: ids} }
+      const cursor2 = await rooms.find(filter)
+      let map = new Map();
+      await cursor2.forEach(e => {
+        map[e._id] = e.name;
+      })
+
+      await cursor.forEach(e => {
+        var aux = new Map();
+        if(id === e.room_1)
+        {
+          aux["_id"] = e._id;
+          aux["name"] = map[e.room_2.toString()];
+          response.push(aux);
+        }
+        else
+        {
+          aux["_id"] = e._id;
+          aux["name"] = map[e.room_1.toString()];
+          response.push(aux);
+        }
+      })
+    } finally {
+      res.send(response);
+    }
+  }
+  run().catch(console.dir);
+});
+
+router.post('/doors', function(req, res) {
+  var room_1 = req.body.room_1;
+  var room_2 = req.body.room_2;
+
+  var door = {
+    room_1: room_1,
+    room_2: room_2
+  };
+
+  async function run() {
+    try {
+      await client.connect();
+
+      const doors = client.db("musipi").collection("doors");
+
+      await doors.insertOne(door);
+    } finally {
+      res.send({success: "Door was added!"});
+    }
+  }
+  run().catch(console.dir);
+});
+
+router.delete('/doors/delete', function(req, res) {
+  var id = req.body.id;
+
+  async function run() {
+    try {
+      await client.connect();
+
+      const doors = client.db("musipi").collection("doors");
+
+      var filter = {'_id': ObjectId(id)}
+      await doors.deleteOne(filter);
+
+    } finally {
+      res.send({success: "Door was deleted!"});
+    }
+  }
+  run().catch(console.dir);
+});
 module.exports = router;
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express();
 const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb");
 const uri = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
@@ -45,27 +46,83 @@ router.post('/rooms', function(req, res) {
 
       await rooms.insertOne(room);
     } finally {
-      res.send(room);
+      res.send({success: "Room was added!"});
     }
   }
   run().catch(console.dir);
 });
 
-router.post('/rooms/activate', function(req, res) {
+router.post('/rooms/edit', function(req, res) {
+  var id = req.body.id;
   var pin = Number(req.body.pin);
   var active = (req.body.active == 'true');
+  var name = req.body.name;
+  var people = req.body.people;
 
   async function run() {
     try {
       await client.connect();
 
       const rooms = client.db("musipi").collection("rooms");
-      const filter = {pin: pin}
-      const update = { $set: { active: active } };
+      const filter = {'_id': ObjectId(id)}
+      var room = await rooms.findOne(filter);
+      if(!req.body.hasOwnProperty('pin'))
+      {
+        pin = room.pin;
+      }
+      if(!req.body.hasOwnProperty('active'))
+      {
+        active = room.active;
+      }
+      if(!req.body.hasOwnProperty('name'))
+      {
+        name = room.name;
+      }
+      if(!req.body.hasOwnProperty('people'))
+      {
+        people = room.people;
+      }
+
+      const update = { $set:
+        {
+          active: active,
+          pin: pin,
+          name: name,
+          people: people
+        } };
 
       await rooms.updateOne(filter, update);
     } finally {
       res.send({success: "Room status changed!"});
+    }
+  }
+  run().catch(console.dir);
+});
+
+router.delete('/rooms/delete', function(req, res) {
+  var id = req.body.id;
+
+  async function run() {
+    try {
+      await client.connect();
+
+      const rooms = client.db("musipi").collection("rooms");
+      const doors = client.db("musipi").collection("doors");
+
+      var filter = {'_id': ObjectId(id)}
+      await rooms.deleteOne(filter);
+
+      filter = {$or: [
+        {
+          room_1: ObjectId(id)
+        },
+        {
+          room_2: ObjectId(id)
+        }
+      ]}
+      await rooms.deleteMany(filter);
+    } finally {
+      res.send({success: "Room was deleted!"});
     }
   }
   run().catch(console.dir);
